c89b8b1fc526e8c232823decbf64823601b79fa0
VARUN RAM <maheshreddym909@gmail.com>
2026-06-23 13:58:06 +0530

    Create CO3,AT3.py

---
+++ CO3,AT3.py
@@ -0,0 +1,50 @@
+NAME: S.VARUN RAM KUMAR
+REG NO: 192565023
+                 1.COMPARING GREEDY AND DYNAMIC PROGRAMMING FOR INVESTMENT PORTFOLIO OPTIMIZATION :
+
+n = int(input())
+
+name = []
+capital = []
+profit = []
+
+for i in range(n):
+    name.append(input())
+    capital.append(int(input()))
+    profit.append(int(input()))
+
+budget = int(input())
+
+# Greedy Method
+ratio = []
+for i in range(n):
+    ratio.append((profit[i] / capital[i], capital[i], profit[i]))
+
+ratio.sort(reverse=True)
+
+g_profit = 0
+g_budget = 0
+
+for r, c, p in ratio:
+    if g_budget + c <= budget:
+        g_budget += c
+        g_profit += p
+
+print("Greedy Profit =", g_profit)
+
+# Dynamic Programming
+w = []
+for i in range(n):
+    w.append(capital[i] // 10000)
+
+b = budget // 10000
+
+dp = [0] * (b + 1)
+
+for i in range(n):
+    for j in range(b, w[i] - 1, -1):
+        dp[j] = max(dp[j], dp[j - w[i]] + profit[i])
+
+print("DP Profit =", dp[b])
+
+2.
